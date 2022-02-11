05400d99592c1ce19e47bc79f75e95980aa7ba75
nazimudheen267 <nazimudheen267@gmail.com>
2022-02-11 12:16:56 +0530

    succesfully added normal-frontend template

--- index.js
+++ index.js
@@ -27,40 +27,83 @@ fs.readdir(`${path.join(__dirname, 'templates')}`, (err, files) => {
         }
     ]
 
-    // for users current directory 
+    // users current directory 
     const CURR_DIR = process.cwd();
 
     inquirer.prompt(QUESTIONS)
         .then(answers => {
-            console.log(answers);
             const projectChoice = answers['project-choice'];
             const projectName = answers['project-name'];
             const templatePath = `${__dirname}/templates/${projectChoice}`;
 
             fs.mkdirSync(`${CURR_DIR}/${projectName}`);
 
-            createDirectoryContents(templatePath, projectName);
+            // getting exact path for the file
+            function formatPath(root, dirName, file) {
+
+                // if dirName is falsy value
+                if (!dirName) return path.format({ dir: root, base: file })
+
+                const exactPath = path.format({
+                    root: root,
+                    dir: dirName,
+                    base: file
+                })
+                return exactPath
+            }
+
+            createDirectoryContents(templatePath)
+                .then(files => {
+
+                    const directories = [];
+
+                    files.forEach((file) => {
+                        const userFile = formatPath(CURR_DIR, projectName, file);
+                        const tempFile = formatPath(templatePath, null, file);
+
+                        // stat 
+                        const stat = fs.statSync(tempFile);
+
+                        if (stat.isFile()) {
+                            const content = fs.readFileSync(tempFile);
+                            fs.writeFileSync(userFile, content);
+                        }
+                        else if (stat.isDirectory()) { directories.push(file) }
+
+                    })
+
+                    return directories;
+
+                }).then(directories => {
+                    directories.forEach((dir) => {
+                        const userPath =  formatPath(CURR_DIR, projectName, dir);
+                        const tempPath = formatPath(templatePath, null, dir);
+                        
+                        // creating directory
+                        fs.mkdirSync(userPath);
+
+                        const readDir = fs.readdirSync(tempPath);
+                        
+                        readDir.forEach(file => {
+                            const stat = fs.statSync(formatPath(tempPath, null, file));
+
+                            if(stat.isFile()){
+                                const content = fs.readFileSync(formatPath(tempPath, null, file));
+
+                                fs.writeFileSync(formatPath(userPath, null, file), content);
+                            }
+                        })
+                    })
+                })
+                .catch(err => console.log(err));
         })
 
     // createDirectoryContents function Definition
-    function createDirectoryContents(templatePath, projectName) {
-        const readFiles = fs.readdirSync(templatePath);
-
-        readFiles.forEach(file => {
-
-            const userFile = `${CURR_DIR}/${projectName}/${file}`;
-
-            const stat = fs.statSync(`${templatePath}/${file}`);
-
-            if (stat.isFile()) {
-                const content = fs.readFileSync(`${templatePath}/${file}`, "utf-8");
-
-                fs.writeFileSync(userFile, content, "utf-8");
-
-            } else if (stat.isDirectory()) {
-                fs.mkdirSync(`${CURR_DIR}/${projectName}/${file}`);
-            }
-        })
+    function createDirectoryContents(templatePath) {
+        return new Promise((resolve, reject) => {
+            fs.readdir(templatePath, (err, success) => err ? reject(err) : resolve(success));
+        });
     }
+
 })
 
